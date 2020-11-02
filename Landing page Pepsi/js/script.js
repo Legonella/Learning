4614ae10e96919955749b710acb269f775110da5
Legonella <dastbravis@gmail.com>
2020-11-02 19:30:44 +0100

    Create a new landing page

---
+++ Landing page Pepsi/js/script.js
@@ -0,0 +1,8 @@
+function img_slider (anything) {
+	document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color) {
+	const container = document.querySelector('.container');
+	container.style.background = color;
+}
