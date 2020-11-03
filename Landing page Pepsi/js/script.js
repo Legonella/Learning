9f2970fa35e432d87a372d0bfa15c7c190fba633
Legonella <dastbravis@gmail.com>
2020-11-03 19:23:45 +0100

    Making the website responsive

--- Landing page Pepsi/js/script.js
+++ Landing page Pepsi/js/script.js
@@ -6,3 +6,10 @@ function changeBgColor(color) {
 	const container = document.querySelector('.container');
 	container.style.background = color;
 }
+
+function menu_toggle () {
+	const toggle_menu = document.querySelector('.toggle_menu');
+	const navigate = document.querySelector('#navigate');
+	toggle_menu.classList.toggle('active');
+	navigate.classList.toggle('active');
+}
